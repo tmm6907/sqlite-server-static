6b3f60b2fde6a1b4151f4e7f58ea8b1032f87d57
Terrence Moore <tmm6907@gmail.com>
2025-02-19 12:24:22 -0500

    refactor and implement alert store

--- frontend/src/components/Editor.svelte
+++ frontend/src/components/Editor.svelte
@@ -2,11 +2,20 @@
     import { onMount } from "svelte";
     import { EditorState } from "@codemirror/state";
     import { EditorView, keymap } from "@codemirror/view";
-    import { defaultKeymap } from "@codemirror/commands";
+    import {
+        defaultKeymap,
+        history,
+        historyKeymap,
+    } from "@codemirror/commands";
     import { sql } from "@codemirror/lang-sql";
     import { HighlightStyle } from "@codemirror/language";
     import { tags } from "@lezer/highlight";
     import { syntaxHighlighting } from "@codemirror/language";
+    import { queryResults } from "../stores/queryResults";
+    import { triggerAlert } from "../stores/alertStore";
+
+    /** @type EditorView*/
+    let editorView;
 
     // Lifecycle function that runs after the component is mounted
     onMount(() => {
@@ -22,25 +31,71 @@
         }
 
         let startState = EditorState.create({
-            doc: "SELECT * FROM users WHERE name ='Ben';",
+            doc: "",
             extensions: [
                 sql(),
-                keymap.of(defaultKeymap),
+                history(),
+                keymap.of([...defaultKeymap, ...historyKeymap]),
                 syntaxHighlighting(customHighlightStyle),
             ],
         });
 
-        let view = new EditorView({
+        editorView = new EditorView({
             state: startState,
             parent: parentElement,
         });
-        view.focus();
+        editorView.focus();
     });
+
+    async function runQuery() {
+        if (!editorView) {
+            console.error("EditorView is not initialized yet.");
+            return;
+        }
+        let query = editorView.state.doc.toString();
+        if (query.length === 0) {
+            triggerAlert("Query cannot be empty!", "alert-danger");
+        }
+        let resp = await fetch("/api/query", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ query: query }),
+        });
+        let res = await resp.json();
+        if (resp.status > 400) {
+            console.error(res.error);
+        }
+        if (res.results) {
+            console.log(res.results);
+            queryResults.set(res.results);
+            editorView.dispatch({
+                changes: {
+                    from: 0,
+                    to: editorView.state.doc.length,
+                    insert: "",
+                },
+            });
+        } else {
+            triggerAlert("Rows affected: ", res.rowsAffected);
+            editorView.dispatch({
+                changes: {
+                    from: 0,
+                    to: editorView.state.doc.length,
+                    insert: "",
+                },
+            });
+        }
+    }
 </script>
 
 <div class="grid grid-cols-5">
     <div class="relative">
-        <button class="btn btn-success absolute bottom-0 right-4">Run</button>
+        <button
+            class="btn btn-success absolute bottom-0 right-4"
+            on:click={runQuery}>Run</button
+        >
     </div>
     <div id="sql-editor"></div>
 </div>
---
+++ frontend/src/components/Table.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { queryResults } from "../stores/queryResults";
+    let rows = [];
+    let cols = [];
+    queryResults.subscribe((value) => {
+        rows = value;
+        cols = rows.length > 0 ? Object.keys(rows[0]) : [];
+    });
+</script>
+
+<table id="main-db" class="table active">
+    <!-- head -->
+    <thead>
+        <tr>
+            {#each cols as col}
+                <th>{col}</th>
+            {/each}
+        </tr>
+    </thead>
+    <tbody>
+        {#each rows as row}
+            <tr class="hover">
+                {#each Object.keys(row) as key}
+                    <td>{row[key]}</td>
+                {/each}
+            </tr>
+        {/each}
+    </tbody>
+</table>
